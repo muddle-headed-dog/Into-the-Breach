0a5f1f852b5917d7ec57a6de8379d8693904fecf
wyb <3464343011@qq.com>
2025-06-08 04:48:31 +0800

    更新场景文件，替换光标和区域的纹理资源；在地图管理脚本中重构光标和区域绘制逻辑，支持动态更新光标位置和区域显示。

--- Scripts/MapManager.gd
+++ Scripts/MapManager.gd
@@ -9,7 +9,7 @@ const cursor_scene = preload("res://Scene/cursor.tscn")
 ## 地图
 @onready var tilemap: TileMapLayer = $Map
 @onready var area_parent: Node2D = $Map/AreaParent
-@onready var cursor: Node2D = $Map/Cursor
+@onready var cursor_parent: Node2D = $Map/CursorParent
 
 ## 寻路网格
 var astar: AStarGrid2D = AStarGrid2D.new()
@@ -24,16 +24,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     if Input.is_action_just_pressed("mouse_left"):
         # 获取屏幕位置（鼠标点击位置）
-        # draw_area(Vector2i(1, 4), Color.RED)
-        # prints("屏幕坐标:", get_viewport().get_mouse_position())
-        # prints("全局坐标:", get_global_mouse_position())
-        # prints("屏幕坐标:", MapTools.logic_global_to_screen(get_global_mouse_position()))
-        # prints("全局坐标:", MapTools.logic_screen_to_global(get_viewport().get_mouse_position()))
-        # prints("地图坐标:", MapTools.global_to_map(get_global_mouse_position(), tilemap))
-        # prints("地图坐标:", MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap))
-        # prints("屏幕坐标:", MapTools.logic_map_to_screen(MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap), tilemap))
-        # prints("全局坐标:", MapTools.map_to_global(MapTools.global_to_map(get_global_mouse_position(), tilemap), tilemap))
-        draw_area(MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap), Color.RED)
+        prints("屏幕坐标:", get_viewport().get_mouse_position())
+        prints("全局坐标:", get_global_mouse_position())
+        prints("屏幕坐标:", MapTools.logic_global_to_screen(get_global_mouse_position()))
+        prints("全局坐标:", MapTools.logic_screen_to_global(get_viewport().get_mouse_position()))
+        prints("地图坐标:", MapTools.global_to_map(get_global_mouse_position(), tilemap))
+        prints("地图坐标:", MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap))
+        prints("屏幕坐标:", MapTools.logic_map_to_screen(MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap), tilemap))
+        prints("全局坐标:", MapTools.map_to_global(MapTools.global_to_map(get_global_mouse_position(), tilemap), tilemap))
+        prints(MapTools.has_tile_at_position(MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap), tilemap))
+        draw_area(PackedVector2Array([MapTools.global_to_map(get_global_mouse_position(), tilemap)]), Color.RED, true)
+        draw_cursor(MapTools.logic_screen_to_map(get_viewport().get_mouse_position(), tilemap), Color.BLUE)
 
 ## 生成地图
 func generate_map() -> void:
@@ -66,12 +67,47 @@ func _draw() -> void:
             draw_circle(global_pos, 5, Color.GREEN)
 
 ## 绘制区域
-func draw_area(map_pos: Vector2i, color: Color) -> void:
-    if not MapTools.has_tile_at_position(map_pos, tilemap):
-        return
-    var area = area_scene.instantiate()
-    var global_pos = MapTools.map_to_global(map_pos, tilemap)
-    # 将全局坐标转换为area_parent的局部坐标
-    area.position = area_parent.to_local(global_pos)
-    area.modulate = color
-    area_parent.add_child(area)
+## 参数：map_pos_array: PackedVector2Array，地图坐标数组
+## 参数：color: Color，颜色
+## 说明：如果区域存在，则更新区域位置，如果区域不存在，则创建区域
+func draw_area(map_pos_array: PackedVector2Array, color: Color, is_update: bool = false) -> void:
+    if is_update:
+        for area in area_parent.get_children():
+                if area.name.begins_with("Area_"):
+                    area.queue_free()
+        draw_area(map_pos_array, color, false)
+    else:
+        for map_pos in map_pos_array:
+            if not MapTools.has_tile_at_position(map_pos, tilemap):
+                return
+            var area = area_scene.instantiate()
+            var tile_offset = MapTools.get_tiles_custom_data(map_pos, tilemap).get("offset", 0)
+            var global_pos = MapTools.logic_get_tile_create_pos(map_pos, tile_offset, tilemap)
+            # 将全局坐标转换为area_parent的局部坐标
+            area.position = area_parent.to_local(global_pos)
+            area.modulate = color
+            area.name = "Area_" + str(map_pos)
+            area_parent.add_child(area)
+
+## 绘制光标 
+## 参数：map_pos: Vector2i，地图坐标
+## 参数：color: Color，颜色
+## 说明：如果光标存在，点击地图外则删除光标，点击地图内则更新光标位置
+## 说明：如果光标不存在，则创建光标
+func draw_cursor(map_pos: Vector2i, color: Color) -> void:
+    if cursor_parent.get_child_count() == 0:
+        var cursor = cursor_scene.instantiate()
+        var tile_offset = MapTools.get_tiles_custom_data(map_pos, tilemap).get("offset", 0)
+        var global_pos = MapTools.logic_get_tile_create_pos(map_pos, tile_offset, tilemap)
+        # 将全局坐标转换为area_parent的局部坐标
+        cursor.position = cursor_parent.to_local(global_pos)
+        cursor.modulate = color
+        cursor.name = "Cursor_" + str(map_pos)
+        cursor_parent.add_child(cursor)
+    else:
+        if MapTools.has_tile_at_position(map_pos, tilemap):
+            cursor_parent.get_child(0).position = cursor_parent.to_local(MapTools.logic_get_tile_create_pos(map_pos, MapTools.get_tiles_custom_data(map_pos, tilemap).get("offset", 0), tilemap))
+            cursor_parent.get_child(0).modulate = color
+            cursor_parent.get_child(0).name = "Cursor_" + str(map_pos)
+        else:
+            cursor_parent.get_child(0).queue_free()
